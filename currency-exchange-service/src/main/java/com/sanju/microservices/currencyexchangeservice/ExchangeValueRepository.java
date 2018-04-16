7896242be63a729759475f0ce06dfede465c034a
sanju784 <sanjunitr51@gmail.com>
2018-04-16 19:25:20 +0900

    Currency Exchange Service

---
+++ currency-exchange-service/src/main/java/com/sanju/microservices/currencyexchangeservice/ExchangeValueRepository.java
@@ -0,0 +1,8 @@
+package com.sanju.microservices.currencyexchangeservice;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ExchangeValueRepository extends JpaRepository<ExchangeValue, Integer>{
+	
+	ExchangeValue findByFromAndTo(String from, String to);
+}
---
+++ currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port)
+values(101,'USD','INR',65,0);
+insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port)
+values(102,'EUR','INR',75,0);
+insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port)
+values(103,'JPY','INR',0.60,0);
